6c2b28247e5cf8dffae72487f52be066a1fff19f
Luka <luka.cavic@gmail.com>
2024-07-11 07:37:25 +0200

    Option to change username in profile page

--- app/Livewire/CustomPersonalInfo.php
+++ app/Livewire/CustomPersonalInfo.php
@@ -4,16 +4,16 @@ namespace App\Livewire;
 
 use Filament\Forms;
 use Filament\Forms\Components\Group;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
+use Illuminate\Validation\Rule;
 use Jeffgreco13\FilamentBreezy\Livewire\PersonalInfo;
 
 class CustomPersonalInfo extends PersonalInfo
 {
     public static $sort = 1;
 
-    public array $only = ['first_name', 'last_name', 'email_signature', 'email', 'avatar_url'];
+    public array $only = ['first_name', 'last_name', 'email_signature', 'email', 'avatar_url', 'name'];
 
     public bool $hasAvatars = true;
 
@@ -28,15 +28,23 @@ class CustomPersonalInfo extends PersonalInfo
     protected function getProfileFormSchema(): array
     {
         $groupFields = Group::make([
-            TextInput::make('first_name')
-                ->label('Ime')
-                ->required(),
+            Forms\Components\Grid::make(2)
+            ->schema([
+                TextInput::make('first_name')
+                    ->label('Ime')
+                    ->required(),
 
-            TextInput::make('last_name')
-                ->required()
-                ->label('Prezime'),
+                TextInput::make('last_name')
+                    ->required()
+                    ->label('Prezime'),
+            ]),
 
-            $this->getEmailComponent(),
+            Forms\Components\Grid::make(2)->schema([
+                $this->getEmailComponent(),
+
+                $this->getNameComponent(),
+            ]),
+
 
             Forms\Components\RichEditor::make('email_signature')
                 ->label('Email potpis'),
@@ -45,5 +53,15 @@ class CustomPersonalInfo extends PersonalInfo
         return ($this->hasAvatars)
             ? [filament('filament-breezy')->getAvatarUploadComponent(), $groupFields]
             : [$groupFields];
+    }
+
+    protected function getNameComponent(): Forms\Components\TextInput
+    {
+        return Forms\Components\TextInput::make('name')
+            ->required()
+            ->rules(Rule::unique('users', 'name')->ignore(auth()->user()->id, 'id')
+                ->where('organisation_id', filament()->getTenant()->id)
+            )
+            ->label('Korisničko ime');
     }
 }
