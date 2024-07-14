3e98f2458452a13ba9c40a13395892243db663bf
Luka <luka.cavic@gmail.com>
2024-07-14 10:16:24 +0200

    Added crypt/decrypt for vaults

--- app/Models/Vault.php
+++ app/Models/Vault.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\Crypt;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -12,8 +13,20 @@ class Vault extends BaseModel implements HasMedia
 {
     use InteractsWithMedia;
 
+    public function setContentAttribute(string $content) {
+        $this->attributes['content'] = Crypt::encryptString($content);
+    }
+
+    public function getContentAttribute($value)
+    {
+        if (is_null($value)) return null;
+
+        return Crypt::decryptString($value);
+    }
+
     public function related(): MorphTo
     {
         return $this->morphTo();
     }
+
 }
